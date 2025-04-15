2a6a3e449b42f8cc29d7589d077a9a2daacda4bd
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2025-04-15 10:36:09 -0400

    Refactor import statements and update version to 0.0.2; remove obsolete test file

--- QuantileFlow/__init__.py
+++ QuantileFlow/__init__.py
@@ -18,10 +18,10 @@ The package includes three main implementations:
 All implementations are designed to handle high-throughput data streams and provide
 accurate quantile estimates with minimal memory overhead.
 """
-from QuantileFlow.momentsketch.core import MomentSketch
-from QuantileFlow.hdrhistogram.core import HDRHistogram
+from .momentsketch.core import MomentSketch
+from .hdrhistogram.core import HDRHistogram
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __all__ = [
     "MomentSketch",
     "HDRHistogram",
